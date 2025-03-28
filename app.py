bb5bf9354b3fb6b78f4a32c8cbcb025bfa795dd5
gganceri <gganceri@gmail.com>
2025-03-27 22:45:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def index():
     return render_template("index.html", respuesta=respuesta)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
