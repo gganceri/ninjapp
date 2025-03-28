e16f9633448e582d93edd5d39025ca8de29dd6a0
gganceri <gganceri@gmail.com>
2025-03-27 23:00:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def index():
             respuesta = "No se recibió ninguna pregunta."
 
     return render_template("index.html", respuesta=respuesta)
-    
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
