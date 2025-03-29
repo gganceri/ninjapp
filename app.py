01e0f666f455bfbe6c039cfd7a8363c3f033b3b7
gganceri <gganceri@gmail.com>
2025-03-29 14:18:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 import os
 from ninjaapp.routes import register_routes
@@ -9,10 +9,12 @@ CORS(app)
 # Registrar rutas desde el módulo
 register_routes(app)
 
+# Página principal
 @app.route("/")
 def home():
-    return "🌐 NinjaApp está corriendo con estructura modular y lista para crecer."
+    return render_template("index.html")
 
+# Puerto para Railway
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(debug=True, host="0.0.0.0", port=port)
